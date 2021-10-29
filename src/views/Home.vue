9f14fe9b1dbc806387102526b0b4522e83edc30c
Seth Delorme <delorme.s.m@gmail.com>
2021-10-28 23:01:16 -0500

    creates nearbySearch method on Home.vue calling backend action to search nearby places based on user input of nearby address, keyword, and type of place

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,58 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Nearby Places</h1>
+    <form v-on:submit.prevent="nearbySearch()">
+      Enter Address:
+      <input type="text" v-model="address" />
+      <br />
+      Enter Keyword:
+      <input type="text" v-model="keyword" />
+      <br />
+      Enter Type:
+      <input type="text" v-model="type" />
+      <br />
+      <input type="submit" value="Search" />
+    </form>
   </div>
 </template>
 
+<style></style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      nearby_places: {},
+      place: {},
+      address: "",
+      keyword: "",
+      type: "",
+    };
+  },
+  created: function () {
+    // this.nearbySearch();
+  },
+  methods: {
+    nearbySearch: function () {
+      var params = {
+        address: this.address,
+        keyword: this.keyword,
+        type: this.type,
+      };
+      axios
+        .get("/api/places/nearby_search", { params })
+        .then((response) => {
+          console.log(params);
+          console.log("nearby search", response);
+          this.nearby_places = response.data;
+        })
+        .catch((error) => {
+          console.log(error.messages);
+        });
+    },
   },
 };
 </script>
