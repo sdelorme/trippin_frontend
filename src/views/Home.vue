40f77bc757e1cfbd19ad817b16cc7578ccaf905e
Seth Delorme <delorme.s.m@gmail.com>
2021-12-13 20:25:35 -0600

    refactor

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -81,7 +81,8 @@ export default {
       nearby_places: {},
       place: {},
       address: "",
-      photo_reference: "",
+      photo_reference:
+        "Aap_uEA7vb0DDYVJWEaX3O-AtYp77AaswQKSGtDaimt3gt7QCNpdjp1BkdM6acJ96xTec3tsV_ZJNL_JP-lqsVxydG3nh739RE_hepOOL05tfJh2_ranjMadb3VoBYFvF0ma6S24qZ6QJUuV6sSRrhCskSBP5C1myCzsebztMfGvm7ij3gZT",
       keyword: "",
       type: "",
       place_id: "",
