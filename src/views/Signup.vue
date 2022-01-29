79c901fa47b0b38895bcb23aaaaf99da9d67827b
Seth Delorme <delorme.s.m@gmail.com>
2022-01-28 22:31:56 -0600

    add v-form component to Signup.vue, refactor Signup.vue to be compatible with new component

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,34 +1,51 @@
 <template>
   <div class="signup">
-    <form v-on:submit.prevent="submit()">
-      <h1>Signup</h1>
-      <ul>
-        <li class="text-danger" v-for="error in errors" v-bind:key="error">
-          {{ error }}
-        </li>
-      </ul>
-      <div class="form-group">
-        <label>Username:</label>
-        <input type="text" class="form-control" v-model="username" />
-      </div>
-      <div class="form-group">
-        <label>Name:</label>
-        <input type="text" class="form-control" v-model="name" />
-      </div>
-      <div class="form-group">
-        <label>Email:</label>
-        <input type="email" class="form-control" v-model="email" />
-      </div>
-      <div class="form-group">
-        <label>Password:</label>
-        <input type="password" class="form-control" v-model="password" />
-      </div>
-      <div class="form-group">
-        <label>Password confirmation:</label>
-        <input type="password" class="form-control" v-model="passwordConfirmation" />
-      </div>
-      <input type="submit" class="btn btn-primary" value="Submit" />
-    </form>
+    <v-form @submit.prevent="submit()" v-model="valid" id="signup">
+      <v-container>
+        <v-row>
+          <v-col cols="12" md="3">
+            <v-text-field v-model="username" :counter="30" label="Username" required></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12" md="3">
+            <v-text-field
+              v-model="firstName"
+              :rules="nameRules"
+              :counter="30"
+              label="First name"
+              required
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12" md="3">
+            <v-text-field v-model="lastName" :rules="nameRules" :counter="30" label="Last name" required></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12" md="3">
+            <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12" md="3">
+            <v-text-field type="password" v-model="password" label="Password" required></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12" md="3">
+            <v-text-field
+              type="password"
+              v-model="passwordConfirmation"
+              label="Confirm Password"
+              required
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-btn form="signup" type="submit" color="success">Sign Up</v-btn>
+      </v-container>
+    </v-form>
   </div>
 </template>
 
@@ -39,18 +56,22 @@ export default {
   data: function () {
     return {
       username: "",
-      name: "",
       email: "",
       password: "",
       passwordConfirmation: "",
       errors: [],
+      valid: false,
+      firstName: "",
+      lastName: "",
+      nameRules: [(v) => !!v || "Name is required", (v) => v.length <= 30 || "Name must be less than 30 characters"],
+      emailRules: [(v) => !!v || "E-mail is required", (v) => /.+@.+/.test(v) || "E-mail must be valid"],
     };
   },
   methods: {
     submit: function () {
       var params = {
         username: this.username,
-        name: this.name,
+        name: `${this.firstName} ${this.lastName}`,
         email: this.email,
         password: this.password,
         password_confirmation: this.passwordConfirmation,
