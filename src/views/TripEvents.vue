6ccf454a659bdc3814effa6dffa0bfad01b02872
Seth Delorme <delorme.s.m@gmail.com>
2022-01-29 14:41:36 -0600

    add deleteTripEvent to TripEvents.vue

--- src/views/TripEvents.vue
+++ src/views/TripEvents.vue
@@ -5,10 +5,12 @@
         <li>
           <a v-bind:href="event.website" target="_blank" rel="noreferrer noopener">{{ event.name }}</a>
         </li>
+        <p>{{ event.id }}</p>
         <p>{{ event.address }}</p>
         <p>{{ event.phone_number }}</p>
         <p>Start:{{ event.start }}</p>
         <p>End:{{ event.end }}</p>
+        <v-btn v-model="event.id" @click="deleteTripEvent(event)">Delete</v-btn>
       </ul>
     </div>
   </div>
@@ -30,6 +32,14 @@ export default {
       this.trip_events = response.data;
     });
   },
-  methods: {},
+  methods: {
+    deleteTripEvent: function (event) {
+      console.log(event.id);
+      axios.delete("api/trip_events/" + event.id).then((response) => {
+        console.log("deleted trip event", response);
+        window.location.reload();
+      });
+    },
+  },
 };
 </script>
