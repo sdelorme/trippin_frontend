0bcdae494ef664ec7008cca4c4b29bda72636c31
Seth Delorme <delorme.s.m@gmail.com>
2022-01-31 21:53:52 -0600

    add nav menu to app.vue with correct routes

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,90 @@
 <template>
-  <v-app id="inspire">
+  <div id="app">
+    <!-- Header -->
+    <header id="header">
+      <div class="inner">
+        <!-- Logo -->
+        <a href="index.html" class="logo">
+          <span class="symbol"><img src="images/logo.svg" alt="" /></span>
+          <span class="title">Phantom</span>
+        </a>
+
+        <!-- Nav -->
+        <nav>
+          <ul>
+            <li><a href="#menu">Menu</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+
+    <nav id="menu">
+      <div class="inner">
+        <h2>Menu</h2>
+        <ul>
+          <li><router-link to="/">Home</router-link></li>
+          <li><router-link v-if="isLoggedIn()" to="/trip_events">My Trip Events</router-link></li>
+          <li><router-link to="/findPlaces">Find Places</router-link></li>
+          <li><router-link to="/calendar">Calendar</router-link></li>
+          <li><router-link v-if="!isLoggedIn()" to="/signup">Sign Up</router-link></li>
+          <li><router-link v-if="!isLoggedIn()" to="/login">Login</router-link></li>
+          <li><router-link v-if="isLoggedIn()" to="/logout">Logout</router-link></li>
+        </ul>
+      </div>
+    </nav>
+
+    <router-view />
+
+    <footer id="footer">
+      <div class="inner">
+        <section>
+          <h2>Get in touch</h2>
+          <form method="post" action="#">
+            <div class="fields">
+              <div class="field half">
+                <input type="text" name="name" id="name" placeholder="Name" />
+              </div>
+              <div class="field half">
+                <input type="email" name="email" id="email" placeholder="Email" />
+              </div>
+              <div class="field">
+                <textarea name="message" id="message" placeholder="Message"></textarea>
+              </div>
+            </div>
+            <ul class="actions">
+              <li><input type="submit" value="Send" class="primary" /></li>
+            </ul>
+          </form>
+        </section>
+        <section>
+          <h2>Follow</h2>
+          <ul class="icons">
+            <li>
+              <a href="#" class="icon brands style2 fa-github"><span class="label">GitHub</span></a>
+            </li>
+            <li>
+              <a href="#" class="icon brands style2 fa-linkedin"><span class="label">LinkedIn</span></a>
+            </li>
+            <li>
+              <a href="#" class="icon solid style2 fa-phone"><span class="label">Phone</span></a>
+            </li>
+            <li>
+              <a href="#" class="icon solid style2 fa-envelope"><span class="label">Email</span></a>
+            </li>
+          </ul>
+        </section>
+        <ul class="copyright">
+          <li>&copy; Untitled. All rights reserved</li>
+          <li>
+            Design:
+            <a href="http://html5up.net">HTML5 UP</a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  </div>
+
+  <!-- <v-app id="inspire">
     <v-app-bar app color="black" dark elevation="1">
       <v-btn text color="accent"><router-link to="/">Home</router-link></v-btn>
       +
@@ -21,21 +106,8 @@
     <v-main>
       <router-view />
     </v-main>
-  </v-app>
+  </v-app> -->
 </template>
-
-<style>
-#nav {
-  padding: 30px;
-}
-#nav a {
-  font-weight: bold;
-  color: #fcfcfc;
-}
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
 
 <script>
 export default {
