22a4794061b5059894181936d1bf86d8e20f509a
Seth Delorme <delorme.s.m@gmail.com>
2022-02-04 20:06:55 -0600

    returned app.vue styling to what it was bu removing a div

--- src/App.vue
+++ src/App.vue
@@ -1,88 +1,87 @@
 <template>
   <div id="app">
-    <div id="wrapper">
-      <header id="header">
-        <div class="inner">
-          <!-- Logo -->
-          <a href="/" class="logo">
-            <span class="symbol"><img src="images/logo.svg" alt="" /></span>
-            <span class="title">Trippin</span>
-          </a>
+    <!-- Header -->
+    <header id="header">
+      <div class="inner">
+        <!-- Logo -->
+        <a href="/" class="logo">
+          <span class="symbol"><img src="images/logo.svg" alt="" /></span>
+          <span class="title">Trippin</span>
+        </a>
 
-          <!-- Nav -->
-          <nav>
-            <ul>
-              <li><a href="#menu">Menu</a></li>
-            </ul>
-          </nav>
-        </div>
-      </header>
-
-      <nav id="menu">
-        <div class="inner">
-          <h2>Menu</h2>
+        <!-- Nav -->
+        <nav>
           <ul>
-            <li><router-link to="/">Home</router-link></li>
-            <li><router-link v-if="isLoggedIn()" to="/trip_events">My Trip Events</router-link></li>
-            <li><router-link to="/findPlaces">Find Places</router-link></li>
-            <li><router-link to="/calendar">Calendar</router-link></li>
-            <li><router-link v-if="!isLoggedIn()" to="/signup">Sign Up</router-link></li>
-            <li><router-link v-if="!isLoggedIn()" to="/login">Login</router-link></li>
-            <li><router-link v-if="isLoggedIn()" to="/logout">Logout</router-link></li>
+            <li><a href="#menu">Menu</a></li>
           </ul>
-        </div>
-      </nav>
+        </nav>
+      </div>
+    </header>
 
-      <router-view />
+    <nav id="menu">
+      <div class="inner">
+        <h2>Menu</h2>
+        <ul>
+          <li><router-link to="/">Home</router-link></li>
+          <li><router-link v-if="isLoggedIn()" to="/trip_events">My Trip Events</router-link></li>
+          <li><router-link to="/findPlaces">Find Places</router-link></li>
+          <li><router-link to="/calendar">Calendar</router-link></li>
+          <li><router-link v-if="!isLoggedIn()" to="/signup">Sign Up</router-link></li>
+          <li><router-link v-if="!isLoggedIn()" to="/login">Login</router-link></li>
+          <li><router-link v-if="isLoggedIn()" to="/logout">Logout</router-link></li>
+        </ul>
+      </div>
+    </nav>
 
-      <footer id="footer">
-        <div class="inner">
-          <section>
-            <h2>Get in touch</h2>
-            <form method="post" action="#">
-              <div class="fields">
-                <div class="field half">
-                  <input type="text" name="name" id="name" placeholder="Name" />
-                </div>
-                <div class="field half">
-                  <input type="email" name="email" id="email" placeholder="Email" />
-                </div>
-                <div class="field">
-                  <textarea name="message" id="message" placeholder="Message"></textarea>
-                </div>
+    <router-view />
+
+    <footer id="footer">
+      <div class="inner">
+        <section>
+          <h2>Get in touch</h2>
+          <form method="post" action="#">
+            <div class="fields">
+              <div class="field half">
+                <input type="text" name="name" id="name" placeholder="Name" />
               </div>
-              <ul class="actions">
-                <li><input type="submit" value="Send" class="primary" /></li>
-              </ul>
-            </form>
-          </section>
-          <section>
-            <h2>Follow</h2>
-            <ul class="icons">
-              <li>
-                <a href="#" class="icon brands style2 fa-github"><span class="label">GitHub</span></a>
-              </li>
-              <li>
-                <a href="#" class="icon brands style2 fa-linkedin"><span class="label">LinkedIn</span></a>
-              </li>
-              <li>
-                <a href="#" class="icon solid style2 fa-phone"><span class="label">Phone</span></a>
-              </li>
-              <li>
-                <a href="#" class="icon solid style2 fa-envelope"><span class="label">Email</span></a>
-              </li>
+              <div class="field half">
+                <input type="email" name="email" id="email" placeholder="Email" />
+              </div>
+              <div class="field">
+                <textarea name="message" id="message" placeholder="Message"></textarea>
+              </div>
+            </div>
+            <ul class="actions">
+              <li><input type="submit" value="Send" class="primary" /></li>
             </ul>
-          </section>
-          <ul class="copyright">
-            <li>&copy; Untitled. All rights reserved</li>
+          </form>
+        </section>
+        <section>
+          <h2>Follow</h2>
+          <ul class="icons">
             <li>
-              Design:
-              <a href="http://html5up.net">HTML5 UP</a>
+              <a href="#" class="icon brands style2 fa-github"><span class="label">GitHub</span></a>
+            </li>
+            <li>
+              <a href="#" class="icon brands style2 fa-linkedin"><span class="label">LinkedIn</span></a>
+            </li>
+            <li>
+              <a href="#" class="icon solid style2 fa-phone"><span class="label">Phone</span></a>
+            </li>
+            <li>
+              <a href="#" class="icon solid style2 fa-envelope"><span class="label">Email</span></a>
             </li>
           </ul>
-        </div>
-      </footer>
-    </div>
+        </section>
+        <ul class="copyright">
+          <li>&copy; Untitled. All rights reserved</li>
+          <li>
+            Design:
+            <a href="http://html5up.net">HTML5 UP</a>
+          </li>
+        </ul>
+      </div>
+    </footer>
   </div>
 </template>
 
