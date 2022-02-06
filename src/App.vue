33d667bf900bef946e51f21fe1a5d8acd90d8e62
Seth Delorme <delorme.s.m@gmail.com>
2022-02-06 12:34:52 -0600

    refactor form in Signup.vue to remove vuetify

--- src/App.vue
+++ src/App.vue
@@ -48,7 +48,7 @@
                   <input type="text" name="name" id="name" placeholder="Name" />
                 </div>
                 <div class="field half">
-                  <input type="email" name="email" id="email" placeholder="Email" />
+                  <input type="email" name="contact-email" id="contact-email" placeholder="Email" />
                 </div>
                 <div class="field">
                   <textarea name="message" id="message" placeholder="Message"></textarea>
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,5 +1,44 @@
 <template>
-  <div class="signup">
+  <div id="wrapper">
+    <div id="main">
+      <div class="inner">
+        <section>
+          <form id="signup" @submit.prevent="submit()">
+            <div class="row gtr-uniform">
+              <div class="col-6 col-12-xsmall">
+                <input type="text" name="first-name" id="first-name" v-model="firstName" placeholder="First Name" />
+              </div>
+              <div class="col-6 col-12-xsmall">
+                <input type="text" name="last-name" id="last-name" v-model="lastName" placeholder="Last Name" />
+              </div>
+              <div class="col-6 col-12-xsmall">
+                <input type="text" name="username" id="username" v-model="username" placeholder="Username" />
+              </div>
+              <div class="col-6 col-12-xsmall">
+                <input type="email" name="email" id="email" v-model="email" placeholder="Email" />
+              </div>
+              <div class="col-6 col-12-xsmall">
+                <input type="password" name="password" id="password" v-model="password" placeholder="Password" />
+              </div>
+              <div class="col-6 col-12-xsmall">
+                <input
+                  type="password"
+                  name="password-confirmation"
+                  id="password-confirmation"
+                  v-model="passwordConfirmation"
+                  placeholder="Confirm Password"
+                />
+              </div>
+              <div class="col-12">
+                <ul class="actions">
+                  <li><input form="signup" type="submit" value="Submit" class="primary" /></li>
+                  <li><input type="reset" value="Clear" /></li>
+                </ul>
+              </div>
+            </div>
+          </form>
+        </section>
+        <!-- <div class="signup">
     <v-form @submit.prevent="submit()" v-model="valid" id="signup">
       <v-container>
         <v-row>
@@ -46,6 +85,9 @@
         <v-btn form="signup" type="submit" color="success">Sign Up</v-btn>
       </v-container>
     </v-form>
+  </div> -->
+      </div>
+    </div>
   </div>
 </template>
 
