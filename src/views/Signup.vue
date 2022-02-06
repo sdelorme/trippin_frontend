2241ca44ba5ee2d33e6dbc7217918e162ec3f4fb
Seth Delorme <delorme.s.m@gmail.com>
2022-02-06 12:36:50 -0600

    refactor signup.vue data

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -2,6 +2,7 @@
   <div id="wrapper">
     <div id="main">
       <div class="inner">
+        <h1>Signup</h1>
         <section>
           <form id="signup" @submit.prevent="submit()">
             <div class="row gtr-uniform">
@@ -38,54 +39,6 @@
             </div>
           </form>
         </section>
-        <!-- <div class="signup">
-    <v-form @submit.prevent="submit()" v-model="valid" id="signup">
-      <v-container>
-        <v-row>
-          <v-col cols="12" md="3">
-            <v-text-field v-model="username" :counter="30" label="Username" required></v-text-field>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col cols="12" md="3">
-            <v-text-field
-              v-model="firstName"
-              :rules="nameRules"
-              :counter="30"
-              label="First name"
-              required
-            ></v-text-field>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col cols="12" md="3">
-            <v-text-field v-model="lastName" :rules="nameRules" :counter="30" label="Last name" required></v-text-field>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col cols="12" md="3">
-            <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col cols="12" md="3">
-            <v-text-field type="password" v-model="password" label="Password" required></v-text-field>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col cols="12" md="3">
-            <v-text-field
-              type="password"
-              v-model="passwordConfirmation"
-              label="Confirm Password"
-              required
-            ></v-text-field>
-          </v-col>
-        </v-row>
-        <v-btn form="signup" type="submit" color="success">Sign Up</v-btn>
-      </v-container>
-    </v-form>
-  </div> -->
       </div>
     </div>
   </div>
@@ -105,8 +58,6 @@ export default {
       valid: false,
       firstName: "",
       lastName: "",
-      nameRules: [(v) => !!v || "Name is required", (v) => v.length <= 30 || "Name must be less than 30 characters"],
-      emailRules: [(v) => !!v || "E-mail is required", (v) => /.+@.+/.test(v) || "E-mail must be valid"],
     };
   },
   methods: {
