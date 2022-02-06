0d00175bf5c6470b384917c5cda72fdc84332795
Seth Delorme <delorme.s.m@gmail.com>
2022-02-06 10:32:08 -0600

    add wrapper to header and footer of app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,24 @@
 <template>
   <div id="app">
     <!-- Header -->
-    <header id="header">
-      <div class="inner">
-        <!-- Logo -->
-        <a href="/" class="logo">
-          <span class="symbol"><img src="images/logo.svg" alt="" /></span>
-          <span class="title">Trippin</span>
-        </a>
+    <div id="wrapper">
+      <header id="header">
+        <div class="inner">
+          <!-- Logo -->
+          <a href="/" class="logo">
+            <span class="symbol"><img src="images/logo.svg" alt="" /></span>
+            <span class="title">Trippin</span>
+          </a>
 
-        <!-- Nav -->
-        <nav>
-          <ul>
-            <li><a href="#menu">Menu</a></li>
-          </ul>
-        </nav>
-      </div>
-    </header>
+          <!-- Nav -->
+          <nav>
+            <ul>
+              <li><a href="#menu">Menu</a></li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </div>
 
     <nav id="menu">
       <div class="inner">
@@ -35,53 +37,55 @@
 
     <router-view />
 
-    <footer id="footer">
-      <div class="inner">
-        <section>
-          <h2>Get in touch</h2>
-          <form method="post" action="#">
-            <div class="fields">
-              <div class="field half">
-                <input type="text" name="name" id="name" placeholder="Name" />
+    <div id="wrapper">
+      <footer id="footer">
+        <div class="inner">
+          <section>
+            <h2>Get in touch</h2>
+            <form method="post" action="#">
+              <div class="fields">
+                <div class="field half">
+                  <input type="text" name="name" id="name" placeholder="Name" />
+                </div>
+                <div class="field half">
+                  <input type="email" name="email" id="email" placeholder="Email" />
+                </div>
+                <div class="field">
+                  <textarea name="message" id="message" placeholder="Message"></textarea>
+                </div>
               </div>
-              <div class="field half">
-                <input type="email" name="email" id="email" placeholder="Email" />
-              </div>
-              <div class="field">
-                <textarea name="message" id="message" placeholder="Message"></textarea>
-              </div>
-            </div>
-            <ul class="actions">
-              <li><input type="submit" value="Send" class="primary" /></li>
+              <ul class="actions">
+                <li><input type="submit" value="Send" class="primary" /></li>
+              </ul>
+            </form>
+          </section>
+          <section>
+            <h2>Follow</h2>
+            <ul class="icons">
+              <li>
+                <a href="#" class="icon brands style2 fa-github"><span class="label">GitHub</span></a>
+              </li>
+              <li>
+                <a href="#" class="icon brands style2 fa-linkedin"><span class="label">LinkedIn</span></a>
+              </li>
+              <li>
+                <a href="#" class="icon solid style2 fa-phone"><span class="label">Phone</span></a>
+              </li>
+              <li>
+                <a href="#" class="icon solid style2 fa-envelope"><span class="label">Email</span></a>
+              </li>
             </ul>
-          </form>
-        </section>
-        <section>
-          <h2>Follow</h2>
-          <ul class="icons">
+          </section>
+          <ul class="copyright">
+            <li>&copy; Untitled. All rights reserved</li>
             <li>
-              <a href="#" class="icon brands style2 fa-github"><span class="label">GitHub</span></a>
-            </li>
-            <li>
-              <a href="#" class="icon brands style2 fa-linkedin"><span class="label">LinkedIn</span></a>
-            </li>
-            <li>
-              <a href="#" class="icon solid style2 fa-phone"><span class="label">Phone</span></a>
-            </li>
-            <li>
-              <a href="#" class="icon solid style2 fa-envelope"><span class="label">Email</span></a>
+              Design:
+              <a href="http://html5up.net">HTML5 UP</a>
             </li>
           </ul>
-        </section>
-        <ul class="copyright">
-          <li>&copy; Untitled. All rights reserved</li>
-          <li>
-            Design:
-            <a href="http://html5up.net">HTML5 UP</a>
-          </li>
-        </ul>
-      </div>
-    </footer>
+        </div>
+      </footer>
+    </div>
   </div>
 </template>
 
