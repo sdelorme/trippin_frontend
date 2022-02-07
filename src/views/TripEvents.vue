cb1d0380a87b75a21773cf6a7a99115f03f53a02
Seth Delorme <delorme.s.m@gmail.com>
2022-02-06 20:24:58 -0600

    convert TripEvents.vue to table format

--- src/views/TripEvents.vue
+++ src/views/TripEvents.vue
@@ -1,17 +1,48 @@
 <template>
-  <div class="home">
-    <div v-for="event in trip_events" :key="event.id">
-      <ul>
-        <li>
-          <a v-bind:href="event.website" target="_blank" rel="noreferrer noopener">{{ event.name }}</a>
-        </li>
-        <p>{{ event.id }}</p>
-        <p>{{ event.address }}</p>
-        <p>{{ event.phone_number }}</p>
-        <p>Start:{{ event.start }}</p>
-        <p>End:{{ event.end }}</p>
-        <v-btn v-model="event.id" @click="deleteTripEvent(event)">Delete</v-btn>
-      </ul>
+  <div id="wrapper">
+    <div id="main">
+      <div class="inner">
+        <div class="table-wrapper">
+          <table class="alt">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Address</th>
+                <th>Phone Number</th>
+                <th>Start Time</th>
+                <th>End Time</th>
+                <th>Delete?</th>
+              </tr>
+            </thead>
+            <tbody v-for="event in trip_events" :key="event.id">
+              <tr>
+                <td>
+                  <a v-bind:href="event.website" target="_blank" rel="noreferrer noopener">{{ event.name }}</a>
+                </td>
+                <td>
+                  {{ event.address }}
+                </td>
+                <td>
+                  {{ event.phone_number }}
+                </td>
+                <td>
+                  {{ event.start }}
+                </td>
+                <td>
+                  {{ event.end }}
+                </td>
+                <td>
+                  <ul class="actions">
+                    <li>
+                      <input type="submit" class="primary" value="Delete" @click="deleteTripEvent(event)" />
+                    </li>
+                  </ul>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   </div>
 </template>
