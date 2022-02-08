2d4729b6e8b1bfcef5d7a1fcdadae74023710305
Seth Delorme <delorme.s.m@gmail.com>
2022-02-07 21:08:08 -0600

    adjust styling of box.alt class to hide overflow text

--- src/views/TripEvents.vue
+++ src/views/TripEvents.vue
@@ -9,7 +9,7 @@
               <tr>
                 <th>Name</th>
                 <th>Address</th>
-                <th>Phone Number</th>
+                <th>Phone</th>
                 <th>Start Time</th>
                 <th>End Time</th>
                 <th>Delete?</th>
