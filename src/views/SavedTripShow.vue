167ad3fd4ede4dac6f1c1b97d0e94f45d2521888
Seth Delorme <delorme.s.m@gmail.com>
2022-02-22 22:00:39 -0600

    render saved_trips on SaveTripShow.vue for temporary view

--- src/views/SavedTripShow.vue
+++ src/views/SavedTripShow.vue
@@ -6,7 +6,20 @@
         <div>
           <p>Id: {{ saved_trip.id }}</p>
           <p>Name: {{ saved_trip.trip_name }}</p>
-          <p>Trip Events: {{ saved_trip.trip_events }}</p>
+          <div v-for="event in saved_trip.trip_events" :key="event.id">
+            <ul>
+              <li>name: {{ event.name }}</li>
+              <li>id: {{ event.id }}</li>
+              <li>place_id: {{ event.place_id }}</li>
+              <li>address: {{ event.address }}</li>
+              <li>phone_number: {{ event.phone_number }}</li>
+              <li>hours: {{ event.hours }}</li>
+              <li>rating: {{ event.rating }}</li>
+              <li>website: {{ event.website }}</li>
+              <li>google_url: {{ event.google_url }}</li>
+              <li>photo_reference: {{ event.photo_reference }}</li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
