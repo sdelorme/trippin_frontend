51fdf305c5f84ccf7869c50d46912b22a473fb0a
Seth Delorme <delorme.s.m@gmail.com>
2022-03-05 10:23:26 -0600

    refactor SaveTripShow.vue to format as table, add back to Saved trips button

--- src/views/SavedTripShow.vue
+++ src/views/SavedTripShow.vue
@@ -2,24 +2,43 @@
   <div id="wrapper">
     <div id="main">
       <div class="inner">
-        <h1>Saved Trip Page</h1>
-        <div>
-          <p>Id: {{ saved_trip.id }}</p>
-          <p>Name: {{ saved_trip.trip_name }}</p>
-          <div v-for="event in saved_trip.trip_events" :key="event.id">
-            <ul>
-              <li>name: {{ event.name }}</li>
-              <li>id: {{ event.id }}</li>
-              <li>place_id: {{ event.place_id }}</li>
-              <li>address: {{ event.address }}</li>
-              <li>phone_number: {{ event.phone_number }}</li>
-              <li>hours: {{ event.hours }}</li>
-              <li>rating: {{ event.rating }}</li>
-              <li>website: {{ event.website }}</li>
-              <li>google_url: {{ event.google_url }}</li>
-              <li>photo_reference: {{ event.photo_reference }}</li>
-            </ul>
-          </div>
+        <h1>{{ saved_trip.trip_name }}</h1>
+        <div class="col-12">
+          <ul class="actions">
+            <li>
+              <router-link to="/saved_trips" tag="button">Back to Saved Trips</router-link>
+            </li>
+          </ul>
+        </div>
+        <div class="table-wrapper">
+          <table class="alt">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Address</th>
+                <th>Phone</th>
+                <th>Rating</th>
+              </tr>
+            </thead>
+            <tbody v-for="event in saved_trip.trip_events" :key="event.id">
+              <tr>
+                <td>
+                  <strong>
+                    <a v-bind:href="event.website" target="_blank" rel="noreferrer noopener">{{ event.name }}</a>
+                  </strong>
+                </td>
+                <td>
+                  {{ event.address }}
+                </td>
+                <td>
+                  {{ event.phone_number }}
+                </td>
+                <td>
+                  {{ event.rating }}
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
