bde0712ff9489d467218b1f09ad88a13b045ba60
Seth Delorme <delorme.s.m@gmail.com>
2022-03-14 20:08:58 -0500

    commented out code of axios interceptors trying to catch 401 eror and leading to lgin page

--- src/main.js
+++ src/main.js
@@ -6,6 +6,21 @@ import axios from "axios";
 axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
 
+// #trying to redirect user to login page when 401 error occurs
+// axios.interceptors.response.use(
+//   (response) => {
+//     console.log("response status", response.status);
+//     return response;
+//   },
+//   (error) => {
+//     if (error.response.status === 401) {
+//       console.log("error status is".error.response.status);
+//       router.push("/login");
+//     }
+//     return Promise.reject(error);
+//   }
+// );
+
 var jwt = localStorage.getItem("jwt");
 if (jwt) {
   axios.defaults.headers.common["Authorization"] = "Bearer " + jwt;
