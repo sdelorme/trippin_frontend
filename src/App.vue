523212ca1b14497d47a375ebd8b852f15be19810
Seth Delorme <delorme.s.m@gmail.com>
2022-03-29 20:47:05 -0500

    remove form from app.vue

--- src/App.vue
+++ src/App.vue
@@ -40,38 +40,25 @@
     <footer id="footer">
       <div class="inner">
         <section>
-          <h2>Get in touch</h2>
-          <form method="post" action="#">
-            <div class="fields">
-              <div class="field half">
-                <input type="text" name="name" id="name" placeholder="Name" />
-              </div>
-              <div class="field half">
-                <input type="email" name="contact-email" id="contact-email" placeholder="Email" />
-              </div>
-              <div class="field">
-                <textarea name="message" id="message" placeholder="Message"></textarea>
-              </div>
-            </div>
-            <ul class="actions">
-              <li><input type="submit" value="Send" class="primary" /></li>
-            </ul>
-          </form>
-        </section>
-        <section>
-          <h2>Follow</h2>
+          <h2>Seth Delorme</h2>
           <ul class="icons">
             <li>
-              <a href="#" class="icon brands style2 fa-github"><span class="label">GitHub</span></a>
+              <a href="https://github.com/sdelorme" class="icon brands style2 fa-github">
+                <span class="label">GitHub</span>
+              </a>
             </li>
             <li>
-              <a href="#" class="icon brands style2 fa-linkedin"><span class="label">LinkedIn</span></a>
+              <a href="www.linkedin.com/in/seth-delorme" class="icon brands style2 fa-linkedin">
+                <span class="label">LinkedIn</span>
+              </a>
             </li>
             <li>
-              <a href="#" class="icon solid style2 fa-phone"><span class="label">Phone</span></a>
+              <a href="tel:8029896323" class="icon solid style2 fa-phone"><span class="label">Phone</span></a>
             </li>
             <li>
-              <a href="#" class="icon solid style2 fa-envelope"><span class="label">Email</span></a>
+              <a href="mailto: delorme.s.m@gmail.com" class="icon solid style2 fa-envelope">
+                <span class="label">Email</span>
+              </a>
             </li>
           </ul>
         </section>
