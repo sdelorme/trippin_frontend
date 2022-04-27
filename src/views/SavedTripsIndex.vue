08c5ab5713756b4dd0e8d1f60a87186c8087749a
Seth Delorme <delorme.s.m@gmail.com>
2022-04-26 21:31:04 -0500

    styling table on My Saved Trips Index, add delete button to table

--- src/views/SavedTripsIndex.vue
+++ src/views/SavedTripsIndex.vue
@@ -3,17 +3,27 @@
     <div id="main">
       <div class="inner">
         <h1>My Saved Trips</h1>
-        <div v-for="(event, i) in saved_trips" :key="i">
-          <article class="box">
-            <router-link v-bind:to="`/saved_trips/${event.id}`">
-              <strong>{{ event.trip_name }}</strong>
-            </router-link>
-            <ul class="actions">
-              <li>
-                <button class="button warning" @click="deleteSavedTrip(i)">Delete This Trip</button>
-              </li>
-            </ul>
-          </article>
+        <div class="table-wrapper">
+          <table class="alt">
+            <thead>
+              <tr>
+                <th>Trip Name</th>
+                <th>Delete?</th>
+              </tr>
+            </thead>
+            <tbody v-for="(event, i) in saved_trips" :key="i">
+              <tr>
+                <td>
+                  <router-link v-bind:to="`/saved_trips/${event.id}`">
+                    <strong>{{ event.trip_name }}</strong>
+                  </router-link>
+                </td>
+                <td>
+                  <button class="button warning small fit" @click="deleteSavedTrip(i)">Delete This Trip</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
